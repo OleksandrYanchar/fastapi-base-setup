720a46d2d891493357b4b3fa20c45c0c955cb8da
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-05-16 22:28:39 +0300

    UPDATE: added redis setup

--- app/configs/db.py
+++ app/configs/db.py
@@ -8,3 +8,8 @@ DB_PASS = os.getenv("DB_PASS")
 DB_USER = os.getenv("DB_USER")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}"
