24103115f006ea0d8420af5a029af44821e40786
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-06-26 23:56:26 +0300

    UPDATE: media dirs setup

--- app/configs/general.py
+++ app/configs/general.py
@@ -1,24 +1,10 @@
-import logging
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-ROOT = os.getenv("ROOT")
+MEDIA_DIR = os.getenv("MEDIA_DIR")
+AVATARS_DIR = f"{MEDIA_DIR}avatars/"
 
-def setup_logger(ROOT):
-    LOG_FILENAME = os.getenv("LOG_FILENAME", "app.log")
-    LOG_FILE = f"{ROOT}/{LOG_FILENAME}"
-
-    log_format = "%(asctime)s %(levelname)-8s %(message)s  file: %(pathname)s  %(funcName)s line %(lineno)d"
-    date_format = "%Y-%m-%d %H:%M:%S"
-    logging.basicConfig(
-        filename=LOG_FILE, level=logging.ERROR, format=log_format, datefmt=date_format
-    )
-
-    # Example logging messages
-    logging.debug("A DEBUG Message")
-    logging.info("An INFO")
-    logging.warning("A WARNING")
-    logging.error("An ERROR")
-    logging.critical("A message of CRITICAL severity")
+os.makedirs(MEDIA_DIR, exist_ok=True)
+os.makedirs(AVATARS_DIR, exist_ok=True)
