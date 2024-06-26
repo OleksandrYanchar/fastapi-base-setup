6d7233c39d00f263fbf44c22ba998e9b9c343762
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-06-26 23:53:43 +0300

    UPDATE: added test media upload

--- app/api/v1/test.py
+++ app/api/v1/test.py
@@ -1,15 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, HTTPException, UploadFile, status
+from configs.general import AVATARS_DIR
+from configs.logger import init_logger
+from tasks.files import upload_picture
 
-from tasks.test import test_celery
+logger = init_logger(__file__)
 
 router = APIRouter(prefix="/test", tags=["test"])
 
+
 @router.get("/")
 async def test():
-    return {"message": "Hello World!"} 
+    return {"message": "Hello World!"}
 
 
-@router.get("/celery")
-async def test():
-    message = test_celery.delay()
-    return {"task_id": message.id, "task_status": message.status}
+@router.post("/add-static")
+async def update_photo(
+    file: UploadFile = File(...),
+):
+
+    file_content = await file.read()
+    filename = file.filename
+
+    # Call the task with file content and filename
+    task_result = upload_picture.delay(file_content, filename, AVATARS_DIR)
+    file_url = task_result.get(timeout=10)
+
+    try:
+        return {"detail": "Image uploaded successfully.", "image": file_url}
+
+    except Exception as e:
+        logger.error(f"Verification error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error occurred during email verification",
+        )
