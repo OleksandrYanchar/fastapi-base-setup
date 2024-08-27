33c17b72689c4e0bbcb1d641fdf8cbd89278fe36
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-08-28 01:41:43 +0300

    ADD: users model

---
+++ advanced/app/postgresql/models/users.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Boolean, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.postgresql.models.base import BaseORM
+from app.postgresql.models.annotes import unique_str_255, uuidpk
+
+class Users(BaseORM):
+    __tablename__ = "users"
+
+    id: Mapped[uuidpk]
+
+    username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
+    emalil: Mapped[unique_str_255]
+
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(64), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(64), nullable=True)
+
+    is_activated: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+
+    avatar: Mapped[str] = mapped_column(String, nullable=True)
