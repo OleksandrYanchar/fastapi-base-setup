4ca22fdf5ee8a5bf5edca2fbd88a3c01fc93d3ac
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-08-28 01:51:37 +0300

    ADD: annotations for frequently used fields

--- advanced/app/postgresql/models/annotes.py
+++ advanced/app/postgresql/models/annotes.py
@@ -11,13 +11,13 @@ uuidpk = Annotated[
 ]
 
 created_at = Annotated[
-    datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    datetime, mapped_column(server_default=text("TIMEZONE('utc', now())")),
 ]
 
 unique_str_255 = Annotated[str, mapped_column(String(255), nullable=False, unique=True)]
 
 optional_str_255 = Annotated[
-    str, mapped_column(String(64), nullable=True, unique=False)
+    str, mapped_column(String(64), nullable=True, unique=False),
 ]
 
 optional_int = Annotated[int, mapped_column(Integer, nullable=True, unique=False)]
