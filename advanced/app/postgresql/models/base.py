0b2b7c28e473277955925cdbd1dbf4aa9a68ff31
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-09-03 01:07:06 +0300

    UPDATE: updated Base

--- advanced/app/postgresql/models/base.py
+++ advanced/app/postgresql/models/base.py
@@ -1,6 +1,20 @@
-from sqlalchemy import MetaData
-from sqlalchemy.orm import declarative_base
+from typing import Any, Dict
+from sqlalchemy import MetaData, inspect
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+import humps
 
+# Define shared metadata
 metadata = MetaData()
 
-BaseORM = declarative_base(metadata=metadata)
+@as_declarative(metadata=metadata)
+class BaseORM:
+    __name__: str
+
+    # Automatically generate table names in snake_case
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return humps.depascalize(cls.__name__)
+
+    # Convert the model instance to a dictionary
+    def dict(self) -> Dict[str, Any]:
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
